a3224c833a6489695c747c9266104edbc9d23dec
yeasin-hossaom <yeasinhossain475@gmail.com>
2021-06-09 22:48:25 +0600

    add shadow

--- src/App.js
+++ src/App.js
@@ -5,34 +5,34 @@ function App() {
   return (
     <div className="flex justify-center p-3">
       <HTMLFlipBook width={300} height={500}>
-        <div className="bg-red-400">
+        <div className="bg-red-400 shadow-md">
           <img className="p-2" src="https://www.programming-hero.com/assets/img/hero-header/iphone/programming-hero.png" alt="" />
         </div>
-        <div className="bg-red-200">
+        <div className="bg-red-200 shadow-md">
           <img className="p-2" src="https://www.programming-hero.com/assets/img/hero-header/iphone/features-v2.png" alt="" />
         </div>
-        <div className="bg-red-300">
+        <div className="bg-red-300 shadow-md">
           <img className="p-2" src="https://www.programming-hero.com/assets/img/hero-header/iphone/features-v3.png" alt="" />
         </div>
-        <div className="bg-red-200">
+        <div className="bg-red-200 shadow-md">
           <img className="p-2" src="https://www.programming-hero.com/assets/img/hero-header/iphone/features-v2.png" alt="" />
         </div>
-        <div className="bg-red-500">
+        <div className="bg-red-500 shadow-md">
           <img className="p-2" src="https://www.programming-hero.com/assets/img/hero-header/iphone/features-v2.png" alt="" />
         </div>
-        <div className="bg-red-400">
+        <div className="bg-red-400 shadow-md">
           <img className="p-2" src="https://www.programming-hero.com/assets/img/hero-header/iphone/programming-hero.png" alt="" />
         </div>
-        <div className="bg-red-200">
+        <div className="bg-red-200 shadow-md">
           <img className="p-2" src="https://www.programming-hero.com/assets/img/hero-header/iphone/features-v2.png" alt="" />
         </div>
-        <div className="bg-red-300">
+        <div className="bg-red-300 shadow-md">
           <img className="p-2" src="https://www.programming-hero.com/assets/img/hero-header/iphone/features-v3.png" alt="" />
         </div>
-        <div className="bg-red-200">
+        <div className="bg-red-200 shadow-md">
           <img className="p-2" src="https://www.programming-hero.com/assets/img/hero-header/iphone/features-v2.png" alt="" />
         </div>
-        <div className="bg-red-500">
+        <div className="bg-red-500 shadow-md">
           <img className="p-2" src="https://www.programming-hero.com/assets/img/hero-header/iphone/features-v2.png" alt="" />
         </div>
 
